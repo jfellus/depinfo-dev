b6627ea5d8bff9fbc9aaa5f411a710dd6ee14d12
jfellus <jfellus.gmail.com>
2016-06-17 16:20:29 +0200

    fixed pb in menu undrop

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,12 @@
 function load_article(article) {
+	$("#menu").addClass("close"); setTimeout(function(){$("#menu").removeClass("close");},500);
+	$("#content").html("<div class='loading'></div>");
 	if(!article) article = "pages/main.html";
 	else article = "pages/articles/"+article+".html";
 	$.get(article, function( res ) {
   		$( "#content" ).html( res );
+	}).error(function(res) {
+		$ ("#content").html("<h1 class='article-not-found'>Article introuvable : "+article+"</h1>");
 	});
 }
 
@@ -20,3 +24,8 @@ window.onhashchange = function() {
 	if(hash[hash.length-1]==='/') hash += "index";
 	open_article(hash);
 }
+
+
+$(function(){
+	window.onhashchange();
+});
